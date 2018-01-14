d02fc70051821aa2bbb9cbc502689df2fce13691
nikuya3 <nikuya@dummy.mail>
2018-01-14 15:18:36 +0100

    Added browser parsing

--- prepare.py
+++ prepare.py
@@ -2,7 +2,8 @@
 import pandas as pd
 from ua_parser.user_agent_parser import ParseUserAgent
 
-ads = pd.read_csv('data/ads.csv', usecols=['click', 'Browser', 'AdvertiserID', 'AdExchange', 'Adslotwidth',
+use_browser = False
+ads = pd.read_csv('data/ads.csv', usecols=['click', 'AdvertiserID', 'AdExchange', 'Adslotwidth',
                                            'Adslotheight', 'imp', 'interest_news',
                                            'interest_eduation', 'interest_automobile', 'interest_realestate',
                                            'interest_IT', 'interest_electronicgame', 'interest_fashion',
@@ -20,7 +21,8 @@ ads = pd.read_csv('data/ads.csv', usecols=['click', 'Browser', 'AdvertiserID', '
                                            'Inmarket_finance', 'Inmarket_travel', 'Inmarket_education',
                                            'Inmarket_service', 'Inmarket_electronicgame', 'Inmarket_book',
                                            'Inmarket_medicine', 'Inmarket_food_drink', 'Inmarket_homeimprovement',
-                                           'Demographic_gender_male', 'Demographic_gender_famale'])
+                                           'Demographic_gender_male', 'Demographic_gender_famale']
+                                          + ['Browser'] if use_browser else [])
 cols = ['click'] + [col for col in ads if col != 'click']
 ads = ads[cols]
 ads.drop(['Unnamed: 0'], axis=1, inplace=True)
@@ -47,8 +49,10 @@ boolean_cols = ['imp', 'click', 'interest_news',
 ads[boolean_cols] = ads[boolean_cols].astype(bool)
 int_cols = ['AdExchange', 'AdvertiserID', 'Adslotwidth', 'Adslotheight']
 ads[int_cols] = ads[int_cols].astype(int)
+if use_browser:
+    ads['Browser'] = ads['Browser'].map(lambda x: ParseUserAgent(x)['family'])
+    ads['Browser'] = ads['Browser'].astype('category')
 ads.to_csv('data/ads_clean.csv')
-ads['Browser'] = ads['Browser'].astype('category')
 ads.head(10)
 ads.info()
 ads.describe()
